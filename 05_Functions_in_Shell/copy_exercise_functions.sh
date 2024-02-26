089721e8a9d2c6c9d492aaa3be45acf13d5c6e3e
Zedds Lloyd <lloydzedds@gmail.com>
2024-02-26 15:25:47 +0530

    Add info in scripts

--- 05_Functions_in_Shell/copy_exercise_functions.sh
+++ 05_Functions_in_Shell/copy_exercise_functions.sh
@@ -1,4 +1,27 @@
-# #!/bin/bash
+#!/bin/bash
+
+# Exercise 2: Modify the script from the previous exercise. Make the "file_count" function accept a directory as an argument. Next have the function display the name of the directory followed by a colon. Finally, display the number of files to the screen on the next line. Call the function three times. First, on the "/etc" directory, next on the "/var" directory and finally on the "/usr/bin" directory.
+# Example output:
+# /etc:
+# 85
+# Resources for this lecture
+
+
+
+# Function to count files in the directories passed as arguments
+function Files_arg() {
+    local LINES1=$(ls "$1" | wc -l)
+    echo "$1:"
+    echo "$LINES1"
+    local LINES2=$(ls "$2" | wc -l)
+    echo "$2:"
+    echo "$LINES2"
+}
+
+# Call the function with two directory PATHHs as arguments
+Files_arg "$@"
+
+
 
 # # Function to count files in the current directory
 # function File_here() {
@@ -20,16 +43,3 @@
 
 # # Call the function for the specified directory
 # Files_count
-
-# Function to count files in the directories passed as arguments
-function Files_arg() {
-    local LINES1=$(ls "$1" | wc -l)
-    echo "$1:"
-    echo "$LINES1"
-    local LINES2=$(ls "$2" | wc -l)
-    echo "$2:"
-    echo "$LINES2"
-}
-
-# Call the function with two directory PATHHs as arguments
-Files_arg "$@"
--- 05_Functions_in_Shell/exercise_functions.sh
+++ 05_Functions_in_Shell/exercise_functions.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+# Practice Exercises
+# Exercise 1: Write a shell script that consists of a function that display the number of files in the present working directory. Name this function "file_count" and call it in your script. If you use a variable in your function, remember to make it a local variable.
+# Hint: The wc utility is used to count the number of lines, words, and bytes.
+
+
 function File_here() {
     local LINES=$(ls | wc -l)
     echo "There are $LINES files in this directory"
